f53f041538be3ab90b686e81c62be5bab32c56f1
Shreya Kumari <shreyasinha3602@gmail.com>
2026-07-27 19:57:37 +0530

    Add pyramid pattern printing program in qa17.cpp
    Implement a program to print a pyramid pattern based on user input for the number of lines.

---
+++ qa17.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number of lines:";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int k=n-i;k>0;k--)//space
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++)//star
+       {
+        cout<<"*";
+       }
+    cout<<endl;
+    }
+    return 0;
+}
